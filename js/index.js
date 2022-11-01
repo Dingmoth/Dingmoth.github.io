dd763c988598e6ae8bc79add631f1016c8652a37
Dingmoth <35528414+Dingmoth@users.noreply.github.com>
2022-11-01 18:04:37 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+
+
+
+// let bgOpinion = document.getElementById("client-opinions");
+// window.addEventListener("scroll" + function(){
+//     bgOpinion.style.backgroundPosition = +window.scrollY+"px";
+// });
+
+
+// function validate(){
+//     if(document.getElementsByClassName("service").checked){       
+//     }  
+//     else{
+//         alert("You must select a service");
+//         return false;
+//     }   
+// }
