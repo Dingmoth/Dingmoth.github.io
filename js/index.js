81a1512f2f0424bd29053e7825405d6b33e1fb39
Dingmoth <35528414+Dingmoth@users.noreply.github.com>
2022-11-02 17:59:35 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,19 +1,7 @@
 'use strict'
 
+function dropMenu() {
+    document.getElementById("drop-buttons").classList.toggle("show");
+}
 
 
-
-// let bgOpinion = document.getElementById("client-opinions");
-// window.addEventListener("scroll" + function(){
-//     bgOpinion.style.backgroundPosition = +window.scrollY+"px";
-// });
-
-
-// function validate(){
-//     if(document.getElementsByClassName("service").checked){       
-//     }  
-//     else{
-//         alert("You must select a service");
-//         return false;
-//     }   
-// }
