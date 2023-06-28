7acef77746c85230e7fd47c6505ebce781519256
Dingmoth <35528414+Dingmoth@users.noreply.github.com>
2023-06-28 20:35:41 +0300

    Add files via upload

--- js/universal.js
+++ js/universal.js
@@ -1,5 +1,20 @@
 'use strict'
 
-function dropMenu() {
-    document.getElementById("drop-buttons").classList.toggle("show");
-}
+const dropdownBtn = document.getElementById("btn");
+const dropdownMenu = document.getElementById("dropdown");
+
+
+const toggleDropdown = function () {
+    dropdownMenu.classList.toggle("show");
+};
+
+dropdownBtn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    toggleDropdown();
+});
+
+document.documentElement.addEventListener("click", function () {
+    if (dropdownMenu.classList.contains("show")) {
+      toggleDropdown();
+    }
+});
